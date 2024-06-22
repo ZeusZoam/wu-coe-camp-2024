0c0d59f113b30925878570b141bc5766edea3956
ZeusZoam <beenzazakung@gmail.com>
2024-06-22 15:37:08 +0700

    add yaimakmak

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+export default function Blog() {
+    return (
+        <div class="flex justify-center">
+            <div class="m-6 h-[100%] w-[1500px] text-center bg-green-400 rounded-lg">
+                <div class="rounded-full"><img src="https://pbs.twimg.com/media/ExGUst2VgAAqJDZ.jpg" class="img-fluid rounded-full " alt="" /></div>
+                <div class="text-3xl font-bold text-red-600">title</div>
+                <div class="text-pink-500  rounded-lg">Yaimakmak</div>
+            </div>
+        </div>
+    )
+}

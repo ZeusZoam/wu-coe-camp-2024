fc1efca3a2e57a81c7d0437a78eae3121d7cff92
ZeusZoam <beenzazakung@gmail.com>
2024-06-22 15:42:10 +0700

    change yaimakmak to hello

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,9 +2,9 @@ export default function Blog() {
     return (
         <div class="flex justify-center">
             <div class="m-6 h-[100%] w-[1500px] text-center bg-green-400 rounded-lg">
-                <div class="rounded-full"><img src="https://pbs.twimg.com/media/ExGUst2VgAAqJDZ.jpg" class="img-fluid rounded-full " alt="" /></div>
+                <img src="https://pbs.twimg.com/media/ExGUst2VgAAqJDZ.jpg" class="img-fluid rounded-full " alt="" />
                 <div class="text-3xl font-bold text-red-600">title</div>
-                <div class="text-pink-500  rounded-lg">Yaimakmak</div>
+                <div class="text-pink-500  rounded-lg">Hello</div>
             </div>
         </div>
     )
